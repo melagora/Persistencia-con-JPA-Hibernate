ffaae00eaf338104d14b4d4529fc560158988a33
Melvin González <54681730+melagora@users.noreply.github.com>
2023-09-14 16:12:15 -0600

    Insert en la entidad

--- src/main/java/com/latam/alura/tienda/pruebas/RegistroDeProducto.java
+++ src/main/java/com/latam/alura/tienda/pruebas/RegistroDeProducto.java
@@ -1,33 +1,26 @@
 package com.latam.alura.tienda.pruebas;
 
-import java.math.BigDecimal;
-
 import javax.persistence.EntityManager;
 
-import com.latam.alura.dao.CategoriaDAO;
-import com.latam.alura.dao.ProductoDAO;
 import com.latam.alura.tienda.modelo.Categoria;
-import com.latam.alura.tienda.modelo.Producto;
 import com.latam.alura.utils.JPAUtils;
 
 public class RegistroDeProducto {
 	public static void main(String[] args) {
 		Categoria celulares = new Categoria("CELULARES");
-		Producto celular = new Producto("Samsung","Telefono usado", new BigDecimal("1000"), celulares);
-		
 		
 		EntityManager em = JPAUtils.getEntityManager();
 		
-		ProductoDAO productoDAO = new ProductoDAO(em);
-		CategoriaDAO categoriaDAO = new CategoriaDAO(em);
 		
 		em.getTransaction().begin();
+		em.persist(celulares);
 		
-		categoriaDAO.guardar(celulares);
-		productoDAO.guardar(celular);
+		celulares.setNombre("LIBROS");
 		
 		//Enviamos los valores a la DB
 		em.getTransaction().commit();
 		em.close();
+		
+		celulares.setNombre("SOFTWARE");
 	}
 }
